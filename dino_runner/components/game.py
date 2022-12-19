a27d568aae81ba4c10385abf47b9b6feb11ace45
Aliz13 <Alisson.Zurita@fundacion-jala.org>
2022-12-19 15:51:59 -0400

    class 4 power_up

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,7 +2,8 @@ import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
-
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
 class Game:
     def __init__(self):
         pygame.init()
@@ -16,10 +17,19 @@ class Game:
         self.y_pos_bg = 380
 
         self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
+        self.points = 0
+        self.power_up_manager = PowerUpManager()
 
+    def score(self):
+        self.points += 1
+        if self.points % 100 == 0:
+            self.game_speed += 1
+
+        print(self.points)
 
     def run(self):
-        # Game loop: events - update - draw
+        
         self.playing = True
         while self.playing:
             self.events()
@@ -35,13 +45,18 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
-
+        self.obstacle_manager.update(self.game_speed, self)
+        self.power_up_manager.update(self.points, self.game_speed, self.player)
+        self.score()
+        self.player.check_invincibility()
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
+        self.power_up_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
---
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -0,0 +1,40 @@
+import pygame
+import random
+from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.utils.constants import (
+        SMALL_CACTUS,
+        LARGE_CACTUS
+        )
+from dino_runner.utils.constants import BIRD
+
+class ObstacleManager:
+
+    def __init__(self):
+        self.obstacles = []
+
+    def update(self, game_speed, game):
+        if len(self.obstacles) == 0:
+            type = random.randint(0, 2)
+            match type:
+                case 0:
+                    self.obstacles.append(Cactus(SMALL_CACTUS))
+                case 1:
+                    self.obstacles.append(Bird(BIRD))
+                case 2:
+                    self.obstacles.append(Cactus(LARGE_CACTUS))
+
+        for obstacle in self.obstacles:
+            obstacle.update(game_speed, self.obstacles)
+            if game.player.dino_rect.colliderect(obstacle.rect):
+                if not game.player.shield:
+
+                    pygame.time.delay(300)
+                    game.playing = False
+                    break
+                else:
+                    self.obstacles.pop()
+
+    def draw(self, screen):
+        for obstacle in self.obstacles:
+            obstacle.draw(screen)
